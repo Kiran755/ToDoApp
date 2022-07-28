f54bd847f0f028d6126754a278c6976ce16667e0
Kiran755 <80158295+Kiran755@users.noreply.github.com>
2022-07-28 13:59:06 +0530

    removed config files

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,7 @@ import {getDatabase} from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCGGHt6rPx8-jcC4eykaPtgvAKMSbXvcYc",
-  authDomain: "todoapp-81598.firebaseapp.com",
-  projectId: "todoapp-81598",
-  storageBucket: "todoapp-81598.appspot.com",
-  messagingSenderId: "776583973328",
-  appId: "1:776583973328:web:3d31ff2523f5b3101c11d5"
+  //confidential
 };
 
 // Initialize Firebase
